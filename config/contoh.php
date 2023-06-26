ff87b4d5b14ac6e3f6b64d2316f6c43bf4eae927
alfian <gusjabungaf@gmail.com>
2023-06-26 08:04:52 +0700

    Configuration

---
+++ config/contoh.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'author' => [
+        'first_name' => 'al',
+        'last_name' => 'fian',
+    ],
+    'email' => 'gusjabungaf@gmail.com',
+    'web' => 'alfiansy.dev'
+];
+
+?>
