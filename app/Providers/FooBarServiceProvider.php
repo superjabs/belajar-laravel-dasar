29ecce5748b567594807ca2f51fc9ccd3e240ed7
alfian <gusjabungaf@gmail.com>
2023-07-26 15:35:12 +0700

    Register Service Provider

---
+++ app/Providers/FooBarServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+
+class FooBarServiceProvider extends ServiceProvider
+{
+
+    public array $singletons = [
+        HelloService::class => HelloServiceIndonesia::class
+    ];
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Foo::class, function($app){
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function($app){
+            return new Bar($app->make(Foo::class));
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
